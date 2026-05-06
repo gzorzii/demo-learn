83d21f0050ea319cfb3386ba27625e407db6efce
gzorzi <gzorzi@ciandt.com>
2026-05-06 10:16:56 -0300

    add frontend

--- src/backend/src/main/java/com/ciet/demo_learn/DemoLearnApplication.java
+++ src/backend/src/main/java/com/ciet/demo_learn/DemoLearnApplication.java
@@ -2,12 +2,8 @@ package com.ciet.demo_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.ciet.demo_learn.config.AppProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
 public class DemoLearnApplication {
 
 	public static void main(String[] args) {
